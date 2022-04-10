0369aa7f524179122036c3a8ec0c53178f88d19e
higashi <naoki.higashi@axas-japan.com>
2022-04-10 17:26:18 +0900

    Chapter2

--- MyApp/MyApp/ViewController.swift
+++ MyApp/MyApp/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 引き算の計算結果をvalue変数に設定
+        let value:Int = 10-1;
+        // `label.textに設定
+        label.text="\(value)";
     }
 
 
